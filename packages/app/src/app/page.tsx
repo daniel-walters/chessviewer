8bfb3885ffb7a798eb09a50963984f8c114cd128
daniel-walters <waltersdaniel96@gmail.com>
2023-07-08 10:48:23 +1000

    Display moves from pre-written pgn

--- packages/app/src/app/page.tsx
+++ packages/app/src/app/page.tsx
@@ -1,6 +1,12 @@
 "use client";
-import styles from "./page.module.scss";
+import { useState } from "react";
+import Image from "next/image";
 
+import { Piece, PieceColour } from "@chessviewer/types";
+import { Move, PGNParser } from "@chessviewer/parser";
+import { Chess } from "@chessviewer/chess";
+
+import pgn from "../test";
 import bPSVG from "../../public/bP.svg";
 import bRSVG from "../../public/bR.svg";
 import bNSVG from "../../public/bN.svg";
@@ -13,15 +19,34 @@ import wNSVG from "../../public/wN.svg";
 import wBSVG from "../../public/wB.svg";
 import wKSVG from "../../public/wK.svg";
 import wQSVG from "../../public/wQ.svg";
-import Image from "next/image";
 
-type Piece = "Pawn" | "Rook" | "Knight" | "Bishop" | "King" | "Queen";
+import styles from "./page.module.scss";
 
-type Colour = "Black" | "White";
+console.log(pgn);
 
-type Pieces = `${Colour}${Piece}`;
+const formatBoard = (board: Chess["board"]): Chess["board"] => {
+  const one = board.slice(0, 8);
+  const two = board.slice(8, 16);
+  const three = board.slice(16, 24);
+  const four = board.slice(24, 32);
+  const five = board.slice(32, 40);
+  const six = board.slice(40, 48);
+  const seven = board.slice(48, 56);
+  const eight = board.slice(56, 64);
 
-const PieceMap: Record<Pieces, string> = {
+  return [
+    ...one.reverse(),
+    ...two.reverse(),
+    ...three.reverse(),
+    ...four.reverse(),
+    ...five.reverse(),
+    ...six.reverse(),
+    ...seven.reverse(),
+    ...eight.reverse(),
+  ];
+};
+
+const PieceMap: Record<Piece, string> = {
   BlackPawn: bPSVG,
   BlackRook: bRSVG,
   BlackKnight: bNSVG,
@@ -36,75 +61,95 @@ const PieceMap: Record<Pieces, string> = {
   WhiteQueen: wQSVG,
 };
 
-function getDefaultBlackSetup(): Pieces[] {
-  const setup: Pieces[] = [
-    "BlackRook",
-    "BlackKnight",
-    "BlackBishop",
-    "BlackQueen",
-    "BlackKing",
-    "BlackBishop",
-    "BlackKnight",
-    "BlackRook",
-  ];
-
-  for (let i = 0; i < 8; i++) {
-    setup.push("BlackPawn");
+const getPlayer = (move: number): PieceColour => {
+  if (move === 0) {
+    return "White";
+  } else if (move === 1) {
+    return "Black";
+  } else {
+    return move % 2 ? "White" : "Black";
   }
-
-  return setup;
-}
-
-function getDefaultWhiteSetup(): Pieces[] {
-  const setup: Pieces[] = [];
-
-  for (let i = 0; i < 8; i++) {
-    setup.push("WhitePawn");
-  }
-
-  setup.push(
-    "WhiteRook",
-    "WhiteKnight",
-    "WhiteBishop",
-    "WhiteQueen",
-    "WhiteKing",
-    "WhiteBishop",
-    "WhiteKnight",
-    "WhiteRook"
-  );
-
-  return setup;
-}
-
-function initBoard() {
-  const board: (null | Pieces)[] = [];
-  const blackSetup = getDefaultBlackSetup();
-  const whiteSetup = getDefaultWhiteSetup();
-
-  board.push(...blackSetup);
-
-  for (let i = 0; i < 32; i++) {
-    board.push(null);
-  }
-
-  board.push(...whiteSetup);
-
-  return board;
-}
+};
 
 export default function Home() {
-  const board = initBoard();
+  const parser = new PGNParser(pgn);
+  const moves = parser.moves;
+  console.log(moves);
+
+  const [chess] = useState(new Chess());
+  const [board, setBoard] = useState(chess.board);
+  const [turn, setTurn] = useState(0);
+  const [move, setMove] = useState(0);
+
+  const handleMove = () => {
+    const turnToShow = moves[turn];
+    const player: PieceColour = getPlayer(move);
+    const moveToShow = turnToShow?.[player === "White" ? 0 : 1];
+    console.log(moveToShow);
+    if (moveToShow) {
+      switch (moveToShow.type) {
+        case Move.MOVE: {
+          if (moveToShow.from && moveToShow.to) {
+            chess.movePiece(moveToShow.from, moveToShow.to);
+          }
+          break;
+        }
+        case Move.CAPTURE: {
+          if (moveToShow.from && moveToShow.to) {
+            chess.movePiece(moveToShow.from, moveToShow.to);
+          }
+          break;
+        }
+        case Move.RESULT: {
+          alert("GAME FINISHED");
+          break;
+        }
+        case Move.CASTLE_LONG: {
+          if (player === "White") {
+            chess.movePiece("e1", "c1");
+            chess.movePiece("a1", "d1");
+          } else if (player === "Black") {
+            chess.movePiece("e8", "c8");
+            chess.movePiece("a8", "d8");
+          }
+          break;
+        }
+        case Move.CASTLE_SHORT: {
+          if (player === "White") {
+            chess.movePiece("e1", "g1");
+            chess.movePiece("h1", "f1");
+          } else if (player === "Black") {
+            chess.movePiece("e8", "g8");
+            chess.movePiece("h8", "f8");
+          }
+          break;
+        }
+      }
+      setBoard([...chess.board]);
+      setMove(move + 1);
+      if (player === "Black") {
+        setTurn(turn + 1);
+      }
+    }
+  };
+
+  const handleClick = () => {
+    handleMove();
+  };
 
   return (
     <main>
       <div className={styles["chess-board"]}>
-        {board.map((piece, i) => {
+        {formatBoard(board).map((piece, i) => {
           return (
             <div key={`${i}`} className={styles["chess-board--square"]}>
-              {piece && <Image alt="" src={PieceMap[piece]} />}
+              {piece && (
+                <Image alt="" src={PieceMap[`${piece.colour}${piece.type}`]} />
+              )}
             </div>
           );
         })}
+        <button onClick={handleClick}>Next Move</button>
       </div>
     </main>
   );
